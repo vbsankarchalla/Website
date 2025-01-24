073a67a926c061b67d5fe09c547a687c5ebf3eda
sankar <cvbsankar@ivp.in>
2025-01-24 21:16:00 +0530

    Simple handle functionality on Button

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,32 +1,7 @@
-import Lists from "./assets/Lists";
-
+import Button from './assets/Button.jsx';
 
 function App() {
-  const fruits = [
-    {id : 1,name :"apple", calories : 95},
-    {id : 2,name :"banana", calories : 45},
-    {id : 3,name :"orenge", calories : 105},
-    {id : 4,name :"coconut", calories : 159},
-    {id : 5,name :"pineapple", calories : 37}
-   ]; 
-   const vegitables = [
-    {id : 1,name :"potatoes", calories : 110},
-    {id : 2,name :"calery", calories : 15},
-    {id : 3,name :"carrots", calories : 25},
-    {id : 4,name :"corn", calories : 63},
-    {id : 5,name :"broccoli", calories : 50}
-   ];
-
-
-
-
-  return(
-    <>
-    {fruits.length > 0 ? <Lists items={fruits} category="Fruits"/> : null}
-    {vegitables.length > 0 ? <Lists items={vegitables} category="vegitables"/> : null}
-    
-    </>
-  );
+  return( <Button />)
 }
 
 export default App
--- my-react-app/src/assets/Button.jsx
+++ my-react-app/src/assets/Button.jsx
@@ -1,8 +1,23 @@
 
 
 function Button(){
+  let count = 0;
+
+
+  const handleClick = (name) => {
+    count += 1;
+    if(count <= 3) {
+      console.log(`${name} you clicked me ${count} time's`);
+    } else {
+      console.log(`${name} Stop clicking me!`);
+    }
+  };
+
+
   return (
-    <button className="button">Click me</button>
+    <button onClick={() => handleClick('Bro')} className="button">
+      Click me 🙂
+    </button>
   );
 }
 
